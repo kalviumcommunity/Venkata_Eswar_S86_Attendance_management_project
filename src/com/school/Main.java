cba6fc8bb8d2e70e526dd50f899600d5457bdab3
unknown <lakshmimadhavi37@gmail.com>
2025-08-02 10:08:41 +0530

    Intialize Project Folder

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -3,5 +3,6 @@ package com.school;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Attendance Management System!");
+        System.out.println("This is the initial setup of the Attendance Management System using Java and OOP principles.");
     }
 }
